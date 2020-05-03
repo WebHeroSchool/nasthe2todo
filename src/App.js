5a2f46e07ad4829fc307ca57dd12b0bff86e5a80
Anastasia Dubovik <nasthe2@gmail.com>
2020-05-03 12:26:36 +0300

    Play with react components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const element = (<div>
-  Hello world!
+const ItemList = () => (<ul>
+  <li>Проснуться в 8:00</li>
+  <li>Почистить зубы</li>
+  <li>Позавтракать</li>
+  <li>Выгулять собаку</li>
+</ul>);
+
+
+const App = () => (<div>
+  <h1>Hello World!</h1>
+  <ItemList />
 </div>);
 
-export { element };
+export default App;
 
