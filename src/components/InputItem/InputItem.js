2182a50560a476b76248d5b74ac992e1188cc8d7
Anastasia Dubovik <nasthe2@gmail.com>
2020-05-11 22:53:45 +0300

    Material UI practice

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<p>InputItem</p>);
+const InputItem = () => (<p>
+	<TextField
+		label="Новое дело"
+    id="outlined-margin-dense"
+    margin="dense"
+    variant="outlined"
+	/>
+</p>);
 
 export default InputItem;
