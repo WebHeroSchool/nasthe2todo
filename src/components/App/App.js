e9e2b07c52539cf6bc6e62669978721e47532326
Anastasia Dubovik <nasthe2@gmail.com>
2020-05-15 16:09:05 +0300

    App.js refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,33 +4,37 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
+class App extends React.Component {
+  render() {
 
-const App = () => {
+  	const items = [
+			{
+				value: 'Встреча с клиентом',
+				isDone: true
+			},
+			{
+  			value: 'Подпись договора',
+  			isDone: true
+  		},
+  		{
+  			value: 'Урок английского',
+  			isDone: false
+  		}
+		];
 
-	const items = [
-		{
-			value: 'Встреча с клиентом',
-			isDone: true
-		},
-		{
-  		value: 'Подпись договора',
-  		isDone: true
-  	},
-  	{
-  		value: 'Урок английского',
-  		isDone: false
-  	}
-	];
-
-	return (<div className={styles.wrap}>
-		<div>
-  		<h1 className={styles.header}>Важные дела:</h1>
-  		<InputItem />
-  		<ItemList items={items}/>
-  		<Footer count={1} />
-  	</div>
-	</div>)
+    return (
+    	<div className={styles.wrap}>
+				<div>
+  				<h1 className={styles.header}>Важные дела:</h1>
+  				<InputItem />
+  				<ItemList items={items}/>
+  				<Footer count={1} />
+  			</div>
+			</div>);	
+	}
 };
+
+
 
 export default App;
 
