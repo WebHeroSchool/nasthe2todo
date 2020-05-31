74a1d8f8bae9fe0d006285daf49be5b6aa0ecd78
Anastasia Dubovik <nasthe2@gmail.com>
2020-05-31 19:16:40 +0300

    Define proptypes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Footer = ({ count }) => (<footer>Количество задач: {count}</footer>);
 
 Footer.defaultProps = {
 	count: 0
+};
+
+Footer.propTypes = {
+  count: PropTypes.number.isRequired
 };
 
 export default Footer;
