499fe8e1fb555e7054e48facc0a6361764367516
Anastasia Dubovik <nasthe2@gmail.com>
2020-06-23 19:57:03 +0300

    Fix tab mess

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -9,39 +9,39 @@ import PropTypes from 'prop-types';
 import styles from './ItemList.module.css';
 
 const ItemList = ({ items, onClickDone, onClickDelete }) => (
-	<ul className={styles.item_list}>
-		{items.map(item => (
-			<li 
-				className={styles.todo_item}
-				key={item.id}>
-				<FormControlLabel
-					control={
-						<Checkbox 
-							checked={item.isDone}
-							color='default'  
-							onClick={() => onClickDone(item.id)}
-						/>
-					}
-					label={
-						<Item 
-							value={item.value} 
-							isDone={item.isDone} 
-							id={item.id}
-							isDeleted={item.isDeleted}
-						/>
-					}
-				/>
-				<DeleteOutlinedIcon onClick={() => onClickDelete(item.id)} />
-			</li>
-		))}
-	</ul>
+  <ul className={styles.item_list}>
+  {items.map(item => (
+    <li 
+      className={styles.todo_item}
+      key={item.id}>
+      <FormControlLabel
+        control={
+          <Checkbox 
+            checked={item.isDone}
+            color='default'  
+            onClick={() => onClickDone(item.id)}
+          />
+        }
+        label={
+          <Item 
+            value={item.value} 
+            isDone={item.isDone} 
+            id={item.id}
+            isDeleted={item.isDeleted}
+          />
+        }
+      />
+    <DeleteOutlinedIcon onClick={() => onClickDelete(item.id)} />
+    </li>
+  ))}
+  </ul>
 );
 
 ItemList.propTypes = {
-	items: PropTypes.array,
-	id: PropTypes.number,
-	onClickDone: PropTypes.func.isRequired,
-	onClickDelete: PropTypes.func.isRequired
+  items: PropTypes.array,
+  id: PropTypes.number,
+  onClickDone: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired
 };
 
 export default ItemList;
