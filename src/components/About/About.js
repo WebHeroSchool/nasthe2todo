646e0cde70b5ced34d75e962b7a4ac86ac56f12b
Anastasia Dubovik <nasthe2@gmail.com>
2020-06-24 21:40:35 +0300

    Add error handling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 import CardContent from '@material-ui/core/CardContent';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Avatar from '@material-ui/core/Avatar';
 import { Octokit } from '@octokit/rest';
-
-import Contacts from '../Contacts/Contacts';
 
 import styles from './About.module.css';
 
@@ -13,40 +12,72 @@ const octokit = new Octokit();
 class About extends React.Component{
   state = {
     isLoading: true,
-    repoList: []
-  }
+    repoList: [],
+    requestFailed: false,
+    error: {}
+  };
 
   componentDidMount() {
-    octokit.repos.listForUser({
-      username: 'nasthe2'
-    }).then(({ data }) => {
-      this.setState({
-        repoList: data,
-        isLoading: false
-      });
-    });
+    octokit.repos
+      .listForUser({
+        username: 'nasthe2'
+      })
+      .then(({ data }) => {
+        this.setState({
+          repoList: data,
+          isLoading: false,
+          avatarUrl: data[0].owner.avatar_url,
+          login: data[0].owner.login
+        })
+      })
+      .catch(err => 
+        this.setState({
+          requestFailed: true,
+          isLoading: false,
+          error: err
+        })
+      )
   }
 
   render() {
-    const { isLoading, repoList } = this.state;
+    const { isLoading, repoList, requestFailed, error, avatarUrl, login } = this.state;
 
     return (
-      <div>
+      <div className={styles.wrap}>
         <CardContent>
-          <Contacts />
-        </CardContent>
-        <CardContent>
-          <h5 className={styles.repoHeader}>{ isLoading ? <CircularProgress /> : 'Мои репозитории' }</h5>
+          {requestFailed && (
+            <div className={styles.error}>
+              <div>{error.name}</div>
+              <div>{error.message}</div>
+            </div>
+          )}
+
+          { isLoading && <CircularProgress /> }
+
           {
-            !isLoading && <ol>
+            !isLoading &&
+            <div>
+              <Avatar
+                alt={repoList.username}
+                src={avatarUrl}
+              />
+              <h5>{login}</h5>
+            </div>
+          }
+
+          {
+            !isLoading && 
+            <ul>
               {
                 repoList.map(repo => (
                   <li key={repo.id}>
-                    {repo.name}
+                    <a href={repo.html_url} className={styles.link}>
+                      {repo.name}
+                    </a>
                   </li>
                 ))
               }
-            </ol>
+            </ul>
           }
         </CardContent>
       </div>
